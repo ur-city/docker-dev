c742552a4522c4635c3efbf9e5748c5c4b8cce51
zsp <zsp@ur.city>
2019-02-24 15:19:15 +0800

    master

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 NOWDIR=$(cd "$(dirname "$0")"; pwd)
 
 apt remove cmdtest
-apt-get install -y software-properties-common curl
+apt-get install -y software-properties-common curl wget
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 add-apt-repository ppa:chris-lea/redis-server -y
@@ -18,7 +18,6 @@ locale-gen zh_CN.UTF-8
 update-locale LC_ALL=zh_CN.UTF-8 LANG=zh_CN.UTF-8
 update-alternatives --set python /usr/bin/python3.7
 
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 cp /usr/share/zoneinfo/Asia/Shanghai  /etc/localtime
 
 cd ~/
@@ -34,6 +33,8 @@ ssh_host github.com
 ssh_host gitee.com
 
 sort -u -o  ~/.ssh/known_hosts ~/.ssh/known_hosts
+
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 
 nvm install 8.9.0
 nvm install node --latest-npm --reinstall-packages-from=node
