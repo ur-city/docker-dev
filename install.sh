38933688bd00e837ca35d7abcd196802ad123cd9
zsp <zsp@ur.city>
2019-02-24 15:17:31 +0800

    master

--- install.sh
+++ install.sh
@@ -3,8 +3,7 @@
 NOWDIR=$(cd "$(dirname "$0")"; pwd)
 
 apt remove cmdtest
-apt-get install -y software-properties-common
-
+apt-get install -y software-properties-common curl
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 add-apt-repository ppa:chris-lea/redis-server -y
